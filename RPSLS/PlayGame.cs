6e71223aef8bc10c0eed22beb0a39adb8242dd77
Andrew K <drummer4life93@yahoo.com>
2020-05-13 14:34:21 -0500

    Added switchcase into CompareGesture method

--- RPSLS/PlayGame.cs
+++ RPSLS/PlayGame.cs
@@ -11,12 +11,15 @@ namespace RPSLS
     {
         public Player playerOne;
         public Player playerTwo;
+        public int playerOneScore = 0;
+        public int playerTwoScore = 0;
 
         public void Intro()
         {
             
             Console.WriteLine("Welcome to Rock Paper Scissors Lizard Spock! \n\nEnter number of human players (0/1/2): ");
             ChooseGameMode();
+            BeginGame();
         }
 
         public void ChooseGameMode()
@@ -53,8 +56,8 @@ namespace RPSLS
 
         public void BeginGame()
         {
-            int playerOneScore = 0;
-            int playerTwoScore = 0;
+            //playerOneScore = 0;
+            //playerTwoScore = 0;
             int roundLimit = 3;
             bool gameOver = false;
 
@@ -70,7 +73,7 @@ namespace RPSLS
                     gameOver = true;
                 }
 
-                else if (playerOneScore == (roundLimit / 2 + 1))
+                else if (playerTwoScore == (roundLimit / 2 + 1))
                 {
                     Console.WriteLine("\nPlayer Two is the winner!");
                     Console.WriteLine($"Player One: {playerOneScore} round wins\n Player Two: {playerTwoScore} round wins");
@@ -86,7 +89,83 @@ namespace RPSLS
 
         public void CompareGestures(Player playerOne, Player playerTwo)
         {
-            
+            switch(playerOne.chosenGesture)
+            {
+                case "rock":
+                    if(playerTwo.chosenGesture == "scissors" || playerTwo.chosenGesture == "lizard")
+                    {
+                        playerOneScore++;
+                    }
+                    else if(playerTwo.chosenGesture == playerOne.chosenGesture)
+                    {
+                        Console.WriteLine("\nIt's a tie!");
+                    }
+                    else
+                    {
+                        playerTwoScore++;
+                    }
+                    break;
+                case "paper":
+                    if (playerTwo.chosenGesture == "rock" || playerTwo.chosenGesture == "Spock")
+                    {
+                        playerOneScore++;
+                    }
+                    else if (playerTwo.chosenGesture == playerOne.chosenGesture)
+                    {
+                        Console.WriteLine("\nIt's a tie!");
+                    }
+                    else
+                    {
+                        playerTwoScore++;
+                    }
+                    break;
+                case "scissors":
+                    if (playerTwo.chosenGesture == "paper" || playerTwo.chosenGesture == "lizard")
+                    {
+                        playerOneScore++;
+                    }
+                    else if (playerTwo.chosenGesture == playerOne.chosenGesture)
+                    {
+                        Console.WriteLine("\nIt's a tie!");
+                    }
+                    else
+                    {
+                        playerTwoScore++;
+                    }
+                    break;
+                case "lizard":
+                    if (playerTwo.chosenGesture == "Spock" || playerTwo.chosenGesture == "paper")
+                    {
+                        playerOneScore++;
+                    }
+                    else if (playerTwo.chosenGesture == playerOne.chosenGesture)
+                    {
+                        Console.WriteLine("\nIt's a tie!");
+                    }
+                    else
+                    {
+                        playerTwoScore++;
+                    }
+                    break;
+                case "Spock":
+                    if (playerTwo.chosenGesture == "rock" || playerTwo.chosenGesture == "scissors")
+                    {
+                        playerOneScore++;
+                    }
+                    else if (playerTwo.chosenGesture == playerOne.chosenGesture)
+                    {
+                        Console.WriteLine("\nIt's a tie!");
+                    }
+                    else
+                    {
+                        playerTwoScore++;
+                    }
+                    break;
+                default:
+                    break;
+                    
+
+            }
         }
 
         public (Player, Player) CreatePlayerCharacters(int humanPlayer)
