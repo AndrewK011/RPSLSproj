afbbf905110ccdc880ec2ade9f36b83d4bdd9f8b
Andrew K <drummer4life93@yahoo.com>
2020-05-13 11:57:39 -0500

    Added CreatePlayerCharacters Method to choose number of players

--- RPSLS/PlayGame.cs
+++ RPSLS/PlayGame.cs
@@ -10,14 +10,66 @@ namespace RPSLS
     {
         public void Intro()
         {
-            Console.WriteLine("Welcome to Rock Paper Scissors Lizard Spock! \n\n Enter number of human players(0/1/2): ");
+            Console.WriteLine("Welcome to Rock Paper Scissors Lizard Spock! \n\n Enter number of human players (0/1/2): ");
         }
 
         public void ChooseGameMode()
         {
-            int gameModeInput; 
-            
-            if()= Console.ReadLine();
+            int gameModeInput;
+
+            if (int.TryParse(Console.ReadLine(), out gameModeInput))
+            {
+                switch(gameModeInput)
+                {
+                    case 0:
+                        CreatePlayerCharacters(gameModeInput);
+                        break;
+                    case 1:
+                        CreatePlayerCharacters(gameModeInput);
+                        break;
+                    case 2:
+                        CreatePlayerCharacters(gameModeInput);
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect input. Enter number of human players (0/1/2):");
+                        ChooseGameMode();
+                        break;
+                }
+                    
+            }
+
+            else
+            {
+                Console.WriteLine("Incorrect input. Enter number of human players (0/1/2):");
+                ChooseGameMode();
+            }         
+        }
+
+        public void CompareGestures()
+        {
+
+        }
+
+        public void CreatePlayerCharacters(int humanPlayer)
+        {
+            if(humanPlayer == 0)
+            {
+                Player playerOne = new Computer();
+                Player playerTwo = new Computer();
+            }
+
+            else if(humanPlayer == 1)
+            {
+                Player playerOne = new Human();
+                Player playerTwo = new Computer();
+
+            }
+            else
+            {
+                Player playerOne = new Human();
+                Player playerTwo = new Human();
+            }
+
         }
     }
 }
